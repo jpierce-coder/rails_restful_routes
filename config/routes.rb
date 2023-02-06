988c7cde418e9375dae3cc380ac6fea1b1fb0f61
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-02-05 19:51:10 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   resources :blogs
+
+  # # index
+  # get 'blogs', to: 'blogs#index'
+
+  # # new
+  # get 'blog/new', to: 'blogs#new'
+
+  # # create
+  # post 'blogs', to: 'blogs#create'
+
+  # # show
+  # get 'blogs/:id', to: 'blogs#show'
+
+  # # edit
+  # get 'blogs/:id/edit', to: 'blogs#edit'
+
+  # # update
+  # put 'blogs/:id', to: 'blogs#update'
+
+  # # destroy
+  # delete 'blogs/:id', to: 'blogs#destroy'
 end
